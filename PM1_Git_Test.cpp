007f33d70fe60b869d368d478d8a34547e00c765
k023g0121desktop <k023g0121@g.neec.ac.jp>
2023-10-11 12:01:04 +0900

    日付の追加

--- PM1_Git_Test.cpp
+++ PM1_Git_Test.cpp
@@ -7,6 +7,8 @@ int main()
 {
     std::cout << "Hello GitHub!\n";
     std::cout << "LC1A_18_ナカヌマ_カツシ\n";
+
+    std::cout << "2023/10/11\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
